9fec977bfd408db93e5fdacefa986354f041095d
Rashida Strozier <rashida.strozier@gmail.com>
2026-02-17 00:48:30 -0500

    hero image, images added, ready to move to portfolio

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,19 @@
-import naySelfImage from "../assets/self/Nay-Self-1.png"
+import heroImage from "../assets/self/Hero-Image.png";
 
 const Hero = () => {
   return (
-    <>
-         <div className="text-3xl font-bold sub-color-2">Naliyah's Art</div>
-      <img src={naySelfImage} />
-    </>
-  )
-}
+    <div
+      className="relative flex items-center justify-center h-[50vh] bg-cover bg-center rounded-lg overflow-hidden"
+      style={{ backgroundImage: `url(${heroImage})` }}
+    >
+      {/* Title */}
+      <div className="relative z-10 text-center">
+        <h1 className="text-4xl sm:text-5xl font-bold sub-color-3">
+          Naliyah's Art
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
